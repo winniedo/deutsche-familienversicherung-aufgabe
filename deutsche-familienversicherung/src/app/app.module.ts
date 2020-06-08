8c3c58b7ebfa17821c166843195365ff6cfdfb46
winnie <pobouh.winnie@gmail.com>
2020-06-08 12:59:24 +0200

    angular/cli update

--- deutsche-familienversicherung/src/app/app.module.ts
+++ deutsche-familienversicherung/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
 import {AppRoutingModule} from './app-routing/app-routing.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-lay4out';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
